18ac29b20a5aa246c146376fb20903837a32e0c5
pontazaricardo <pontaza.ricardo@gmail.com>
2017-10-13 12:18:39 +0800

    Update

--- main/light_motor/light_motor.ino
+++ main/light_motor/light_motor.ino
@@ -3,7 +3,8 @@
 Servo myservo;  // create servo object to control a servo
 
 void setup() {
-
+  myservo.attach(8);  // attaches the servo on pin 8 to the servo object
+  Serial.begin(9600);
 }
 
 void loop(){

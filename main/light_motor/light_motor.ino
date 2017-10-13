45e4cfbcd69f734293370ec288c9eb7ffbbd0417
pontazaricardo <pontaza.ricardo@gmail.com>
2017-10-13 12:19:21 +0800

    Update

--- main/light_motor/light_motor.ino
+++ main/light_motor/light_motor.ino
@@ -1,6 +1,9 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo;        // create servo object to control a servo
+
+int potpin = A0;      // analog pin used to connect the potentiometer
+int val;              // variable to read the value from the analog pin
 
 void setup() {
   myservo.attach(8);  // attaches the servo on pin 8 to the servo object

29f8e3edfa9af9ddca3c2371c9fe74d5d1dcc07c
pontazaricardo <pontaza.ricardo@gmail.com>
2017-10-13 12:20:32 +0800

    Update

--- main/light_motor/light_motor.ino
+++ main/light_motor/light_motor.ino
@@ -14,6 +14,8 @@ void loop(){
   val = analogRead(potpin);         // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 3500, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
 
+  Serial.println(val);
+  myservo.write(val);               // sets the servo position according to the scaled value
 
 
   
